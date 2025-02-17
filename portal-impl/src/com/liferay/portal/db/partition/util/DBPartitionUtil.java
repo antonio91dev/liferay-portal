d70809e6749e1866cafd03c84d8f0a811ed7bf5a
Alberto Chaparro <alberto.chaparro@liferay.com>
2025-02-17 18:02:51 +0100

    LPD-47382 Bug copying object tables in PostgreSQL

--- portal-impl/src/com/liferay/portal/db/partition/util/DBPartitionUtil.java
+++ portal-impl/src/com/liferay/portal/db/partition/util/DBPartitionUtil.java
@@ -550,7 +550,8 @@ public class DBPartitionUtil {
 								"companyId = ", fromCompanyId));
 					}
 
-					if (fromTableName.startsWith("Object") &&
+					if (StringUtil.startsWith(
+							StringUtil.lowerCase(fromTableName), "object") &&
 						dbInspector.hasColumn(fromTableName, "dbTableName")) {
 
 						statement.executeUpdate(
