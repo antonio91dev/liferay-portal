b20bfe997e3d3a489f6bb78ee78848185eea5d6f
Igor Franca <igor.franca@liferay.com>
2025-05-19 22:28:04 -0300

    LPD-55285 Change DDM tests

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-field-type/src/test/java/com/liferay/dynamic/data/mapping/form/field/type/internal/util/DDMFormFieldTemplateContextContributorUtilTest.java
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-field-type/src/test/java/com/liferay/dynamic/data/mapping/form/field/type/internal/util/DDMFormFieldTemplateContextContributorUtilTest.java
@@ -7,8 +7,7 @@ package com.liferay.dynamic.data.mapping.form.field.type.internal.util;
 
 import com.liferay.dynamic.data.mapping.form.field.type.internal.checkbox.CheckboxDDMFormFieldTemplateContextContributor;
 import com.liferay.dynamic.data.mapping.model.DDMFormField;
-import com.liferay.dynamic.data.mapping.test.util.BaseDDMFormFieldTypeSettingsTestCase;
-import com.liferay.portal.kernel.json.JSONArray;
+import com.liferay.dynamic.data.mapping.test.util.BaseDDMFormFieldTemplateContextContributorTestCase;
 import com.liferay.portal.kernel.json.JSONObject;
 import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.test.rule.LiferayUnitTestRule;
@@ -45,15 +44,11 @@ public class DDMFormFieldTemplateContextContributorUtilTest
 			checkboxDDMFormFieldTemplateContextContributor.getParameters(
 				ddmFormField, createDDMFormFieldRenderingContext());
 
-		JSONArray availableLocalesJSONArray = (JSONArray)parameters.get(
-			"availableLocales");
 		JSONObject defaultLocaleJSONObject = (JSONObject)parameters.get(
 			"defaultLocale");
 		JSONObject editingLocaleJSONObject = (JSONObject)parameters.get(
 			"editingLocale");
 
-		Assert.assertEquals(
-			availableLocales.size(), availableLocalesJSONArray.length());
 		Assert.assertEquals(
 			LocaleUtil.US.toString(), defaultLocaleJSONObject.get("localeId"));
 		Assert.assertEquals(
