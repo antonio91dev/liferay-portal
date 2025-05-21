2692e79209fb7bb2ff769f09d0830c6032c5e539
Brian Chan <brian.chan@liferay.com>
2025-05-21 12:53:16 -0300

    LPD-54384 Wordsmith
    (cherry picked from commit 850f03e6a0e6d46163a51ed72c4779634a8894ba)

--- modules/apps/headless/headless-delivery/headless-delivery-test/src/testIntegration/java/com/liferay/headless/delivery/resource/v1_0/test/StructuredContentResourceTest.java
+++ modules/apps/headless/headless-delivery/headless-delivery-test/src/testIntegration/java/com/liferay/headless/delivery/resource/v1_0/test/StructuredContentResourceTest.java
@@ -2602,9 +2602,7 @@ public class StructuredContentResourceTest
 				});
 		}
 
-		Assert.assertTrue(
-			structuredContent2 + " does not equal " + putStructuredContent,
-			_equals(structuredContent2, putStructuredContent));
+		Assert.assertTrue(_equals(structuredContent2, putStructuredContent));
 
 		_assertLocalizedValues(
 			putStructuredContent,
