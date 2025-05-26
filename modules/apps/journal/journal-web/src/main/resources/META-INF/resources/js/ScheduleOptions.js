2dba59f025ad3dcfd99628cedc49617e08d6ffd5
MADDI VAMSHI KRISHNA <vamshi.krishna@liferay.com>
2025-05-26 14:07:56 +0530

    LPD-56362 Remove past date Validation
    (cherry picked from commit e694787e0cd466ac813dec09249dcc6931475a4a)

--- modules/apps/journal/journal-web/src/main/resources/META-INF/resources/js/ScheduleOptions.js
+++ modules/apps/journal/journal-web/src/main/resources/META-INF/resources/js/ScheduleOptions.js
@@ -30,17 +30,6 @@ export default function ScheduleOptions({
 
 				return;
 			}
-
-			const date = new Date(displayDate);
-
-			if (
-				date <=
-				new Date(new Date().toLocaleString('en-US', timeZone.id))
-			) {
-				setError(
-					Liferay.Language.get('the-date-entered-is-in-the-past')
-				);
-			}
 			else {
 				setError('');
 			}
