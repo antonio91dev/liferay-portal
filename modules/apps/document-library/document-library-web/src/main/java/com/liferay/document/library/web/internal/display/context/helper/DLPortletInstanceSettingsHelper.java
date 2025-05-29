e7f5295955b13d54f708366dcf62c2ee0df3ce91
Jürgen Kappler <jurgen.kappler@liferay.com>
2025-05-29 13:56:53 +0200

    LPD-56625 Document type should also be in the list

--- modules/apps/document-library/document-library-web/src/main/java/com/liferay/document/library/web/internal/display/context/helper/DLPortletInstanceSettingsHelper.java
+++ modules/apps/document-library/document-library-web/src/main/java/com/liferay/document/library/web/internal/display/context/helper/DLPortletInstanceSettingsHelper.java
@@ -145,7 +145,7 @@ public class DLPortletInstanceSettingsHelper {
 	}
 
 	private String[] _getAllEntryColumns() {
-		String allEntryColumns = "name,description,size,status";
+		String allEntryColumns = "name,description,document-type,size,status";
 
 		if (ViewCountManagerUtil.isViewCountEnabled(
 				ClassNameLocalServiceUtil.getClassNameId(
