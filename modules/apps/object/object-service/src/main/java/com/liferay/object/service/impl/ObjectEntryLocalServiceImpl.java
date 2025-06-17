f80afc96882a65289d344bab890c0d4154e7b07b
Anderson Luiz <anderson.luiz@liferay.com>
2025-06-17 19:03:59 -0300

    LPD-57356 SF Use GetterUtil#get

--- modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectEntryLocalServiceImpl.java
+++ modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectEntryLocalServiceImpl.java
@@ -4215,9 +4215,7 @@ public class ObjectEntryLocalServiceImpl
 		values.put(columnName + "i18n", (Serializable)localizedValues);
 		values.putIfAbsent(
 			StringUtil.removeLast(columnName, StringPool.UNDERLINE),
-			String.valueOf(
-				localizedValues.getOrDefault(
-					defaultLanguageId, StringPool.BLANK)));
+			GetterUtil.get(localizedValues.get(defaultLanguageId), StringPool.BLANK));
 	}
 
 	private void _putObjectFilterParser(
