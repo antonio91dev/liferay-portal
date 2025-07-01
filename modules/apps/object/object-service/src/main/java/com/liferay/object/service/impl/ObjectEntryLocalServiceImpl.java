b829fd545e9297d5e8c28c602cbde61673743a45
Mario Gomes <mario.gomes@liferay.com>
2025-07-01 10:03:22 -0300

    LPD-58589 Fix many-to-many query relationship support cross-scoped

--- modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectEntryLocalServiceImpl.java
+++ modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectEntryLocalServiceImpl.java
@@ -3045,7 +3045,11 @@ public class ObjectEntryLocalServiceImpl
 				objectRelationship.getCompanyId()
 			).and(
 				() -> {
-					if (groupId == 0) {
+					if (Objects.equals(
+							objectDefinition2.getScope(),
+							ObjectDefinitionConstants.SCOPE_COMPANY) ||
+						(groupId == 0)) {
+
 						return null;
 					}
 
