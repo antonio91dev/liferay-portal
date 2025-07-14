429d1e47152f09feefbbb004b9669e7237166cf4
Lourdes Fernández Besada <lourdes.fernandez@liferay.com>
2025-07-14 17:59:16 +0200

    LPD-60259 Restore the test expected result since it's asserting the LPS-159512 expected behavior

--- modules/apps/layout/layout-test/src/testIntegration/java/com/liferay/layout/service/test/LayoutLocalServiceCopyLayoutContentTest.java
+++ modules/apps/layout/layout-test/src/testIntegration/java/com/liferay/layout/service/test/LayoutLocalServiceCopyLayoutContentTest.java
@@ -495,7 +495,7 @@ public class LayoutLocalServiceCopyLayoutContentTest {
 				getLayoutClassedModelUsagesByPlid(targetLayout.getPlid());
 
 		Assert.assertEquals(
-			layoutClassedModelUsages.toString(), 0,
+			layoutClassedModelUsages.toString(), 1,
 			layoutClassedModelUsages.size());
 	}
 
