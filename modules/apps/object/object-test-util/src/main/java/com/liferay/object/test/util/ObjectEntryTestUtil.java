0a2c12ce0e8d840b263dc7fae83e11431aa21756
Carolina Barbosa <carolina.barbosa@liferay.com>
2025-08-12 08:15:21 -0300

    LPD-62104 Implement new methods and classes to simplify integration tests

---
+++ modules/apps/object/object-test-util/src/main/java/com/liferay/object/test/util/ObjectEntryTestUtil.java
@@ -0,0 +1,30 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2025 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.object.test.util;
+
+import com.liferay.object.model.ObjectDefinition;
+import com.liferay.object.model.ObjectEntry;
+import com.liferay.object.service.ObjectEntryLocalServiceUtil;
+import com.liferay.portal.kernel.test.util.ServiceContextTestUtil;
+import com.liferay.portal.kernel.test.util.TestPropsValues;
+
+import java.util.Collections;
+
+/**
+ * @author Carolina Barbosa
+ */
+public class ObjectEntryTestUtil {
+
+	public static ObjectEntry addObjectEntry(ObjectDefinition objectDefinition)
+		throws Exception {
+
+		return ObjectEntryLocalServiceUtil.addObjectEntry(
+			TestPropsValues.getUserId(), 0,
+			objectDefinition.getObjectDefinitionId(), Collections.emptyMap(),
+			ServiceContextTestUtil.getServiceContext());
+	}
+
+}
