e68519fe4efdd88099b7414e5f0f204ed0d85d2e
Manuele Castro <manuele.castro@liferay.com>
2025-08-19 15:08:31 -0300

    LPD-63202 Only enable portal/saml/acs when it's a post method

--- modules/dxp/apps/saml/saml-web/src/main/java/com/liferay/saml/web/internal/struts/AssertionConsumerServiceAction.java
+++ modules/dxp/apps/saml/saml-web/src/main/java/com/liferay/saml/web/internal/struts/AssertionConsumerServiceAction.java
@@ -24,6 +24,8 @@ import com.liferay.saml.runtime.servlet.profile.WebSsoProfile;
 
 import java.io.IOException;
 
+import java.util.Objects;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
@@ -38,8 +40,10 @@ import org.osgi.service.component.annotations.Reference;
 public class AssertionConsumerServiceAction extends BaseSamlStrutsAction {
 
 	@Override
-	public boolean isEnabled() {
-		if (_samlProviderConfigurationHelper.isRoleSp()) {
+	public boolean isEnabled(HttpServletRequest httpServletRequest) {
+		if (_samlProviderConfigurationHelper.isRoleSp() &&
+			Objects.equals(httpServletRequest.getMethod(), "POST")) {
+
 			return _samlProviderConfigurationHelper.isEnabled();
 		}
 
