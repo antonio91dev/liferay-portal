96399074768d2c2b1309502e39a27b6d196f02f5
Georgel Pop <georgel.pop@liferay.com>
2025-08-29 14:04:32 +0200

    LPD-64119 Inline replace with pattern variable

--- modules/apps/fragment/fragment-entry-processor/fragment-entry-processor-drop-zone/src/main/java/com/liferay/fragment/entry/processor/drop/zone/DropZoneDocumentFragmentEntryProcessor.java
+++ modules/apps/fragment/fragment-entry-processor/fragment-entry-processor-drop-zone/src/main/java/com/liferay/fragment/entry/processor/drop/zone/DropZoneDocumentFragmentEntryProcessor.java
@@ -154,15 +154,11 @@ public class DropZoneDocumentFragmentEntryProcessor
 						layoutStructure.getLayoutStructureItem(dropZoneItemId);
 
 					if (!(childLayoutStructureItem instanceof
-							FragmentDropZoneLayoutStructureItem)) {
+							FragmentDropZoneLayoutStructureItem
+								fragmentDropZoneLayoutStructureItem)) {
 
 						continue;
 					}
-
-					FragmentDropZoneLayoutStructureItem
-						fragmentDropZoneLayoutStructureItem =
-							(FragmentDropZoneLayoutStructureItem)
-								childLayoutStructureItem;
 
 					String fragmentDropZoneId =
 						fragmentDropZoneLayoutStructureItem.
