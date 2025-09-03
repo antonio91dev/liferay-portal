e1d7b35279fffde8cd82a41b1b2eef6c799fe943
Lianne Louie <lianne.louie@liferay.com>
2025-09-03 00:21:06 -0700

    LPD-61751 add service layer call

--- portal-impl/src/com/liferay/portal/service/impl/OrganizationLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portal/service/impl/OrganizationLocalServiceImpl.java
@@ -63,6 +63,7 @@ import com.liferay.portal.kernel.service.WebsiteLocalService;
 import com.liferay.portal.kernel.service.persistence.CompanyPersistence;
 import com.liferay.portal.kernel.service.persistence.CountryPersistence;
 import com.liferay.portal.kernel.service.persistence.GroupPersistence;
+import com.liferay.portal.kernel.service.persistence.OrganizationUtil;
 import com.liferay.portal.kernel.service.persistence.RegionPersistence;
 import com.liferay.portal.kernel.service.persistence.UserFinder;
 import com.liferay.portal.kernel.service.persistence.UserPersistence;
@@ -929,6 +930,10 @@ public class OrganizationLocalServiceImpl
 
 		return organizationFinder.countO_U_ByC_P(
 			companyId, parentOrganizationId, queryDefinition);
+	}
+
+	public List<Organization> getOrganizationsByLogoId(long logoId) {
+		return organizationPersistence.findByLogoId(logoId);
 	}
 
 	/**
