26e9000ad70b50c858ce285a002b5e5da4516811
Brian Chan <brian.chan@liferay.com>
2025-09-08 12:53:20 -0300

    LPD-61879 Can I change it to this?

--- modules/apps/layout/layout-impl/src/main/java/com/liferay/layout/internal/importer/structure/util/FragmentLayoutStructureItemImporter.java
+++ modules/apps/layout/layout-impl/src/main/java/com/liferay/layout/internal/importer/structure/util/FragmentLayoutStructureItemImporter.java
@@ -1438,7 +1438,7 @@ public class FragmentLayoutStructureItemImporter
 		return jsonObject;
 	}
 
-	private static final String _NAMESPACE_PLACEHOLDER = "[namespace]";
+	private static final String _NAMESPACE_PLACEHOLDER = "[$NAMESPACE$]";
 
 	private static final Log _log = LogFactoryUtil.getLog(
 		FragmentLayoutStructureItemImporter.class);
