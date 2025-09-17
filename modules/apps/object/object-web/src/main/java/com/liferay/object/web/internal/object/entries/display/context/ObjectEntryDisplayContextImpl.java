2228bb87cb627a7ed12ec0e1bbb4872b84a249bc
Carolina Barbosa <carolina.barbosa@liferay.com>
2025-09-17 07:54:57 -0300

    LPD-64702 Simplify by setting the label's default locale to the same locale as the current value

--- modules/apps/object/object-web/src/main/java/com/liferay/object/web/internal/object/entries/display/context/ObjectEntryDisplayContextImpl.java
+++ modules/apps/object/object-web/src/main/java/com/liferay/object/web/internal/object/entries/display/context/ObjectEntryDisplayContextImpl.java
@@ -747,7 +747,7 @@ public class ObjectEntryDisplayContextImpl
 			new DDMFormField(fieldName, DDMFormFieldTypeConstants.FIELDSET) {
 				{
 					setLabel(
-						new LocalizedValue() {
+						new LocalizedValue(_objectRequestHelper.getLocale()) {
 							{
 								addString(
 									_objectRequestHelper.getLocale(), label);
