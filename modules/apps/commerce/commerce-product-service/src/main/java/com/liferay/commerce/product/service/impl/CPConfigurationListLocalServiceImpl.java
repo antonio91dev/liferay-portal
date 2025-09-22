e20602f995ca724e546add5a545aaf9f059c8036
Brian I. Kim <il.kim@liferay.com>
2025-09-23 08:34:14 +0900

    LPD-66322 Fix ordering of parameters

--- modules/apps/commerce/commerce-product-service/src/main/java/com/liferay/commerce/product/service/impl/CPConfigurationListLocalServiceImpl.java
+++ modules/apps/commerce/commerce-product-service/src/main/java/com/liferay/commerce/product/service/impl/CPConfigurationListLocalServiceImpl.java
@@ -231,8 +231,8 @@ public class CPConfigurationListLocalServiceImpl
 				return cpConfigurationListLocalService.
 					updateCPConfigurationList(
 						externalReferenceCode,
-						cpConfigurationList.getCPConfigurationListId(), groupId,
-						userId, parentCPConfigurationListId, master, name,
+						cpConfigurationList.getCPConfigurationListId(), userId,
+						groupId, parentCPConfigurationListId, master, name,
 						priority, displayDateMonth, displayDateDay,
 						displayDateYear, displayDateHour, displayDateMinute,
 						expirationDateMonth, expirationDateDay,
