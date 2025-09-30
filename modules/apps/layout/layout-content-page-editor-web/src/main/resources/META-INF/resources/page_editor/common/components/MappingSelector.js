f439d58dd6627c44c737ac15f140c59f8a656e55
Sandro Chinea <sandrochinea@gmail.com>
2025-09-30 16:00:09 +0100

    LPD-66947 Unify logic

--- modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/common/components/MappingSelector.js
+++ modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/common/components/MappingSelector.js
@@ -346,7 +346,9 @@ function MappingSelector({
 	const {selectedMappingTypes} = config;
 
 	const [itemFields, setItemFields] = useState(null);
-	const [selectedItem, setSelectedItem] = useState(mappedItem);
+	const [selectedItem, setSelectedItem] = useState(
+		getHydratedItem(mappedItem, pageContents)
+	);
 
 	const [typeLabel, setTypeLabel] = useState(null);
 	const [subtypeLabel, setSubtypeLabel] = useState(null);
@@ -403,7 +405,9 @@ function MappingSelector({
 	}, [fieldType, relationships, selectedMappingTypes]);
 
 	const onInfoItemSelect = (selectedInfoItem) => {
-		setSelectedItem(selectedInfoItem);
+		const item = getHydratedItem(selectedInfoItem, pageContents);
+
+		setSelectedItem(item);
 
 		if (isMapped(mappedItem)) {
 			onMappingSelect({});
@@ -445,17 +449,6 @@ function MappingSelector({
 		setTypeLabel(type);
 		setSubtypeLabel(subtype);
 	}, [selectedItem, pageContents]);
-
-	useEffect(() => {
-		if (isMappedToInfoItem(mappedItem)) {
-			const pageContent = findPageContent(pageContents, mappedItem);
-
-			setSelectedItem({
-				...pageContent,
-				...mappedItem,
-			});
-		}
-	}, [mappedItem, pageContents, setSelectedItem]);
 
 	useEffect(() => {
 		if (
@@ -643,6 +636,16 @@ function MappingSelector({
 			/>
 		</>
 	);
+}
+
+function getHydratedItem(item, pageContents) {
+	const pageContent = findPageContent(pageContents, item);
+
+	if (pageContent) {
+		return {...pageContent, ...item};
+	}
+
+	return item;
 }
 
 MappingSelector.propTypes = {
